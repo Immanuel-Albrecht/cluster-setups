65bd8ba3c57eea1f45fbde14bb662ecea77cd20a
Immanuel Albrecht <mail@immanuel-albrecht.de>
2019-04-04 08:35:14 +0200

    fixed delete script

--- hetzner/delete-my-cluster.sh
+++ hetzner/delete-my-cluster.sh
@@ -2,9 +2,9 @@
 
 cd $(dirname $0)
 
-MINIONS="dave stuart jerry jorge tim mark phil kevin bob" # jon is missing here...
+MINIONS="dave stuart jerry jorge tim mark phil kevin bob jon"
 
 ./delete-mini-server.sh
-for name in MINIONS ; do
+for name in $MINIONS ; do
 ./delete-micro-server.sh $name
 done
