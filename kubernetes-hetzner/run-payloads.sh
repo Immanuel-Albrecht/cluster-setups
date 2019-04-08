4f2cd2dd2bcbddb8bd347e6a0ad900124b8a61c9
Immanuel Albrecht <albrecht@coraixx.com>
2019-04-08 10:39:35 +0200

    retrying on run-payloads.sh

--- kubernetes-hetzner/run-payloads.sh
+++ kubernetes-hetzner/run-payloads.sh
@@ -2,8 +2,35 @@
 
 TARGET=root@$1
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r payloads $TARGET:'~'
+for ((retries=0; retries<20; retries+= 1 )) ; do
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $TARGET 'chmod +x payloads/*'
+if scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r payloads $TARGET:'~' ; then
+    break
+fi
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $TARGET "tmux new-session -d -s setup-session $2"
+echo "Retrying ... ($retries)"
+sleep 10
+
+done
+
+for ((retries=0; retries<20; retries+= 1 )) ; do
+
+if ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $TARGET 'chmod +x payloads/*' ; then
+    break
+fi
+
+echo "Retrying ... ($retries)"
+sleep 10
+
+done
+
+for ((retries=0; retries<20; retries+= 1 )) ; do
+
+if ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $TARGET "tmux new-session -d -s setup-session $2" ; then
+    break
+fi
+
+echo "Retrying ... ($retries)"
+sleep 10
+
+done
